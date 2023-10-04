1f2d4a407d0287524c5ba85843edcbf05034b72a
ichi <ichino9824@gmail.com>
2023-10-04 15:43:22 +0900

    no message

---
+++ exp/views.py
@@ -0,0 +1,55 @@
+import json
+import os
+import csv
+from flask import render_template
+from flask import render_template, request
+from exp import app
+from flask import Flask, request, Response
+
+@app.route('/')
+def index():
+    return render_template('exp/index.html')
+
+@app.route('/top1')
+def top1():
+    return render_template('exp/top1.html')
+
+@app.route('/examine1')
+def examine1():
+    return render_template('exp/examine1.html')
+
+
+@app.route('/end')
+def end():
+    return render_template('exp/end.html')
+
+@app.route('/end', methods=['GET'])
+def end_form():
+    return render_template('exp/end.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    try:
+        raw_data = request.form.to_dict()
+        suffix = raw_data["file_name_suffix"]
+        for data_name in ["user_data", "predictions", "estimations"]:
+            data = json.loads(raw_data[data_name])
+            file_name = data_name + "_" + suffix
+            filepath = os.path.join('.', 'res_' + file_name + '.csv')
+
+            # res.csvが存在しないとき，作成し，ヘッダーを書き込む
+            if not os.path.exists(filepath):
+                with open(filepath, 'w', newline='') as f:
+                    writer = csv.DictWriter(f, data[0].keys())
+                    writer.writeheader()
+
+            with open(filepath, 'a', newline='') as f:
+                writer = csv.DictWriter(f, data[0].keys())
+                writer.writerows(data)
+
+        return Response(status=200)
+
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        return Response(status=500)
